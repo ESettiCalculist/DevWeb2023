d5033a6aa966cc6c1a707aa6302a3b9d2b2e28a9
Erick <ericksettidossantos@hotmail.com>
2023-05-10 21:51:59 -0300

    Aula 04 JS

---
+++ Erick 7/WORKSPACE/JS/aula4js/main.js
@@ -0,0 +1,12 @@
+var numero = 11000045485739
+
+
+function verificarNumero() {
+    if (numero % 2  == 0) {
+        console.log(numero + " é par.");
+    } else {
+        console.log(numero + " é impar.");
+    }
+}
+
+verificarNumero()
