3170015c53d06fce880084424b7bd7b761b6f4af
Erick <ericksettidossantos@hotmail.com>
2023-05-11 21:49:39 -0300

    aula 05 js

---
+++ Erick 7/WORKSPACE/JS/aula5js/exercicio da aula/main.js
@@ -0,0 +1,24 @@
+let i = 1
+let t = 10
+let f = 0
+
+while (i <= 50) {
+    console.log(i)
+    i++
+}
+
+console.log("Prox. ex.")
+
+do {
+  console.log(t)
+  t--
+} while (t >= 0)
+
+console.log("Prox. ex.")
+
+for (f = 0;f <= 100; f++) {
+    if (f % 2 === 0) {
+    console.log(f)
+    i++
+    }
+}
