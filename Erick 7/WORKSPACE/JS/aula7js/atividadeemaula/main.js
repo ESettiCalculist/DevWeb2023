abdfba662a34d901a3be125da605557ce494a2bf
Erick <ericksettidossantos@hotmail.com>
2023-05-22 18:53:31 -0300

    Atualizando repositorio do curso para entrega de exercicios

---
+++ Erick 7/WORKSPACE/JS/aula7js/atividadeemaula/main.js
@@ -0,0 +1,12 @@
+//Objetos
+let pessoa = {
+    nome: "erick",
+    idade: 27,
+    altura: 1.74
+}
+
+
+console.table(pessoa)
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.altura)
