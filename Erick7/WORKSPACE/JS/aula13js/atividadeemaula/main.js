bd12599f78369b196db578b7597a37f1339461ef
Erick <ericksettidossantos@hotmail.com>
2023-06-05 21:20:34 -0300

    atualização repositorio

---
+++ Erick7/WORKSPACE/JS/aula13js/atividadeemaula/main.js
@@ -0,0 +1,11 @@
+// Método getElementById
+let titulo = document.getElementById('titulo');
+// Alterando o conteúdo do elemento
+titulo.innerHTML = 'Olá meus caros alunos!';
+// Configurando o estilo CSS do elemento
+titulo.style.textAlign = 'center';
+titulo.style.backgroundColor = '#CCCCC9';
+titulo.style.borderBottom = 'solid 3px #000';
+titulo.style.margin = '20px';
+
+console.log("titulo")
